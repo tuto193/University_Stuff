91663b55ec22bbcf3753cdcf6024326ee698382a
tuto193 <carlos.a.parra.f@gmail.com>
2018-04-13 23:48:56 +0200

    Die wichtigsten Tests sind schon da...

--- Übungsblatt_01/StringStack/TestMethodes.java
+++ Übungsblatt_01/StringStack/TestMethodes.java
@@ -10,12 +10,23 @@ public class TestingMethodes{
         namesList.push( anotherName );
 
         // Reference copy
-        StringStack referenceList = referenceCopy( namesList );
+        //StringStack referenceList = new StringStack( namesList );
 
         // Shallow copy
-        StringStack shallowList = shallowCopy( namesList );
+        //StringStack shallowList = new StringStack( namesList );
 
         // Deep copy
-        StringStack deepList = deepCopy( namesList );
+        StringStack deepList = new StringStack( namesList );
+
+        /*
+            Hier wollen wir Testen, ob es eingentlich um eine Deep-Copy handelt!
+        */
+        if( deepList == namesList ) {
+            throw new RuntimeException( "Es ist eine Reference Copy!" );
+        } else if( deepList.first == namesList.first ) {
+            throw new RuntimeException( "Es ist eine Shallow Copy " );
+        } else if( deepList.first.equals( namesList.first ) ) {
+            throw new RuntimeException( "Es ist eine DEEP COPY! " );
+        }
     }
 }
