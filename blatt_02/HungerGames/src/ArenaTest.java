6bc9eeffb57f49b221bba1a237d8a5478e9218b5
tuto193 <carlos.a.parra.f@gmail.com>
2018-04-20 16:47:53 +0200

    Already made some nice  tests... maybe we still need to expand on them

--- blatt_02/HungerGames/src/ArenaTest.java
+++ blatt_02/HungerGames/src/ArenaTest.java
@@ -1,12 +1,54 @@
 public class ArenaTest {
+    /**
+     * The method takes an x and y coordinate, an expected value and a result
+     * It will test the expected against the result and give errors accordingly
+     * Otherwise, it will just print result with its given coordinates
+     * 
+     * @param x x coordinate
+     * @param y y coordinate
+     * @param expected the expected result from the operation
+     * @param result the actual resulting value of the operation
+     * @throws RuntimeException if the expected and result are not equal
+     */
+    public static void autoTest( double x, double y, int expected, int result ) {
+        if( expected != result ) {
+            throw new RuntimeException("At ("+x+","+y +") expected to be at "+expected+
+                    ", but is at "+result );
+        }
+        if( result > 0 ){
+            System.out.printf( "Tribute in (%f,%f) is in Area %d\n", x, y, result );
+        } else if( result < 0 ) {
+            System.out.printf( "Tribute died in (%f,%f) :( \n", x, y );
+        } else {
+            System.out.println( "Tribute is RIGHT in the centre of the arena" );
+        }
+    }
+
     public static void main( String[] args ) {
         Arena myArena = new Arena();
-        System.out.println( "The results of 0,1 is: " + myArena.getArea(0,1) );
-        System.out.println( "The results of 1,1 is: " + myArena.getArea(1,1) );
-        System.out.println( "The results of 1,0 is: " + myArena.getArea(1,0) );
-        System.out.println( "The results of 0,0 is: " + myArena.getArea(0,0) );
-        System.out.println( "The results of -1,1 is: " + myArena.getArea(-1,1) );
-        System.out.println( "The results of 0.5,1 is: " + myArena.getArea(0.5,1) );
-        System.out.println( "The results of -1,0.5 is: " + myArena.getArea(-1,0.5) );
+
+        int test1 = myArena.getArea( 0, 1 ); // up
+        int test2 = myArena.getArea( 0, 0 ); // centre
+        int test3 = myArena.getArea( 1.5, 1.5 ); // top right corner            
+        int test4 = myArena.getArea( 1, 1 ); // 2?
+        
+        autoTest(0, 1, 12, test1);
+        autoTest(0, 0, 0, test2);
+        autoTest(1.5, 1.5, -1, test3);
+        autoTest(1, 1, 2, test4);
+        
+        /* int width = 30;
+        int height = 30;
+        int[][] myArea = new int[width][height];
+
+        for( int i = 0; i < width; i++ ) {
+            for( int j = 0; j < height; j++ ) {
+                double coorX = ((double) i - 15)/ 10;
+                double coorY = ((double) j - 15)/ 10;
+                int sector = myArena.getArea( coorX, coorY );
+                myArea[i][j] = sector;
+                System.out.printf("At [%f,%f] is the tribute in area: %d\n", coorX, coorY, sector );
+            }
+        } */
     }
 }
