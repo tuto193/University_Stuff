d3df02aba7aae5d2d4b494a0c92b78eb331c7581
tuto193 <carlos.a.parra.f@gmail.com>
2018-04-22 11:08:48 +0200

    It is all nice and Javadocumented now

--- blatt_02/Fraction/src/Calculator.java
+++ blatt_02/Fraction/src/Calculator.java
@@ -1,8 +1,26 @@
 import java.util.regex.Pattern;
 
+/**
+ * This is just a test class for our fractions, to make sure that all operations
+ * work from the command line and that the user input is well parsed.
+ */
 public class Calculator {
+    /**
+     * Just a global regex, for parsing the desired user input
+     */
     public static final String MYREGEX = "-?\\d*/\\d*\\s+[\\+\\-\\*/]\\s+-?\\d*/\\d*\\s?"; 
 
+    /**
+     * This method will just take an input String, which should contain an
+     * operation between two fractions. It will split the input accordingly,
+     * do the operation chosen by the user, then output the result.
+     * 
+     * @param input a String, which should contain two Fractions and an operator
+     *              (+, -, /, *)
+     * @throws RuntimeException if the input String does not contain exactly two
+     *          Fractions, a valid operation in between them,
+     *           and a space separating them from each other.
+     */
     public static void calculate( String input ) {
         if( !input.matches(MYREGEX) ) {
             throw new RuntimeException( "No fractions and operator. Two" 
@@ -39,6 +57,13 @@ public class Calculator {
         }
     }
 
+    /**
+     * There is the user input read from the command line and checked for the
+     * first time against the regex. This will warn the user if the input string
+     * is valiud or not.
+     * 
+     * @throws System.err if the user does not input a String in the valid format.
+     */
     public static void main( String[] args ) {
         String input = new String();
         for( int i = 0; i < args.length; i++ ) {
