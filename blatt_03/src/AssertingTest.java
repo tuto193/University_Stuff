7370d02151c1545645021d9cfd7d363ae0b8db02
tuto193 <carlos.a.parra.f@gmail.com>
2018-05-03 10:49:40 +0200

    Added some more tests. Need o pay attention

--- blatt_03/src/AssertingTest.java
+++ blatt_03/src/AssertingTest.java
@@ -1,6 +1,6 @@
 public class AssertingTest {
     
-    public static void printErrorCheck( String whatError, boolean passed, int testNumber ) {
+    public static void printErrorCheck( String whatError, boolean passed, String testNumber ) {
         if( passed ) {
             System.out.println( "The check on " + whatError + " at " + testNumber + " was a PASS." );
         } else {
@@ -13,9 +13,18 @@ public class AssertingTest {
         Person secondPerson = new Student( "Jesus ", 1);
         Student firstStudent = new Student( "Jesus ", 1);
         // We test first if it's reflexive!
-        boolean test1 = firstPerson.equals(firstPerson);
-        boolean test2 = secondPerson.equals(secondPerson);
-        boolean test3 = firstStudent.equals(firstStudent);
-        //
+        boolean Reflextest2 = secondPerson.equals(secondPerson);
+        boolean Reflextest1 = firstPerson.equals(firstPerson);
+        boolean Reflextest3 = firstStudent.equals(firstStudent);
+        // We then test whether they are actually symmetric!
+        boolean symmTest1 = firstPerson.equals(secondPerson) && secondPerson.equals(firstPerson);
+        boolean symmTest2 = firstPerson.equals(firstStudent) && firstStudent.equals(firstPerson);
+        boolean symmTest3 = secondPerson.equals(firstStudent) && firstStudent.equals(secondPerson);
+        // Then we also Test whether the comparisson is also transtitive
+        //  from firstPerson to first Student
+        boolean transTest1 = firstPerson.equals(secondPerson) && secondPerson.equals(firstStudent) && firstPerson.equals(firstStudent);
+        boolean transTest2 = secondPerson.equals(firstStudent) && firstStudent.equals(firstPerson) && secondPerson.equals(firstPerson);
+
+
     }
 }
