88d00345a19b6ad4168b263c9d05e284b29a8145
tuto193 <carlos.a.parra.f@gmail.com>
2018-05-06 21:32:56 +0200

    More changes

---
+++ blatt_03/src/Point.java
@@ -0,0 +1,17 @@
+public class Point extends Geometry {
+    private double[] coordinates;
+    
+    public Point( double[] coordinates ) {
+        super( coordinates.length );
+        this.coordinates = coordinates;
+    }
+
+    public Point( int dimensions ) {
+        super( dimensions );
+        double[] iniCoord = new double[dimensions];
+        for( int i = 0; i < iniCoord.length; i++ ) {
+            iniCoord[i] = 0;
+        }
+        this.coordinates = iniCoord;
+    }
+}
--- blatt_03/src/Point2D.java
+++
@@ -1,3 +0,0 @@
-public class Point2D extends Geometry {
-    
-}
