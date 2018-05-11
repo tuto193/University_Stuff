ce8fd12ed3c3875b81493619a26de38fd899bbf8
tuto193 <carlos.a.parra.f@gmail.com>
2018-05-11 13:12:02 +0200

    Added Files from previous excercise

---
+++ blatt_04/Calculator.java
@@ -0,0 +1,154 @@
+/**
+ * Contains a main class to execute a Calculate instance, which calculates an
+ * operation on two Fraction instances.
+ *
+ * @author Mathias Menninghaus (mathias.menninghaus@uos.de)
+ */
+public class Calculator {
+
+   /*
+    * The Operators
+    */
+   public static final String ADD = "+";
+   public static final String SUBSTRACT = "-";
+   public static final String MULTIPLY = "*";
+   public static final String DIVIDE = "/";
+
+   /**
+    * @param args
+    */
+   public static void main(String[] args) {
+
+      if (args.length != 3) {
+         System.err.println("invalid number of arguments (must be three)");
+         printUsage();
+         System.exit(1);
+      } else {
+         Calculator calc = new Calculator();
+         String result = calc.calc(args[0], args[1], args[2]);
+         if (result == null) {
+            System.err.println(calc.getErrorMessage());
+            printUsage();
+            System.exit(1);
+         } else {
+            System.out.println("= " + result);
+         }
+      }
+   }
+
+   /**
+    * Prints a short description of the usage on the standard console.
+    */
+   private static void printUsage() {
+      System.out.println("Usage: java Calculator fraction operator fraction");
+      System.out.println("a fraction is defined by " + Fraction.REGEX);
+      System.out.println("valid operators are +,-, *, /");
+   }
+
+   /**
+    * Holds the error message of the last call of
+    * {@link #calc(String, String, String)} which went wrong.
+    */
+   private String errorMessage;
+
+   /**
+    * Calculates the formula given by <code>a operator b</code>. If a, operator
+    * or b are not valid, null will be returned and errorMessage will hold a
+    * description of the error that occurred.
+    *
+    * @param a        the first Fraction argument
+    * @param operator operator to connect the arguments with
+    * @param b        the second Fraction argument
+    * @return The result of the operation as Fraction or null.
+    */
+   private Fraction calc(Fraction a, String operator, Fraction b) {
+
+      Fraction result;
+      /*
+       * differentiate between operators and compute regarding operation.
+       */
+      switch (operator) {
+
+         case ADD:
+            result = a.add(b);
+            break;
+         case SUBSTRACT:
+            result = a.subtract(b);
+            break;
+         case MULTIPLY:
+            result = a.multiply(b);
+            break;
+         case DIVIDE:
+            if (b.getNumerator() == 0) {
+               this.errorMessage = "divides zero";
+               return null;
+            } else {
+               result = a.divide(b);
+            }
+            break;
+         default:
+            this.errorMessage = "Operation " + operator + " unknown";
+            return null;
+      }
+
+      return result;
+
+   }
+
+   /**
+    * Calculates the formula given by <code>a operator b</code>. If a, operator
+    * or b are not valid, null will be returned and errorMessage will hold a
+    * description of the error that occurred.
+    *
+    * @param a        String representation of the first argument
+    * @param operator operator to connect the arguments with
+    * @param b        String representation of the second argument
+    * @return The result of the operation as String or null.
+    */
+   public String calc(String a, String operator, String b) {
+
+      Fraction fractionA = parseFraction(a);
+      Fraction fractionB = parseFraction(b);
+
+      if (fractionA == null || fractionB == null || operator == null) {
+         return null;
+      }
+
+      Fraction result = calc(fractionA, operator, fractionB);
+
+      if (result == null) {
+         return null;
+      }
+
+      return result.toString();
+
+   }
+
+   /**
+    * Return the error message of the last call of
+    * {@link #calc(String, String, String)} which went wrong.
+    *
+    * @return the last error message
+    */
+   public String getErrorMessage() {
+      return this.errorMessage;
+   }
+
+   /**
+    * Parses the given String to a Fraction and returns it. If it cannot be
+    * parsed, null will be returned and errorMessage will hold a description of
+    * the error that occurred.
+    *
+    * @param fraction String to be parsed to a fraction
+    * @return A Fraction representing the given String or null.
+    */
+   private Fraction parseFraction(String fraction) {
+      if (!fraction.matches(Fraction.REGEX)) {
+         errorMessage = fraction + " is not a valid Fraction";
+         return null;
+      }
+      return Fraction.parseFraction(fraction);
+
+   }
+
+}
---
+++ blatt_04/Fraction.java
@@ -0,0 +1,193 @@
+/**
+ * Every instance of Fraction represents a fraction with numerator and
+ * denominator.
+ * 
+ * @author Mathias Menninghaus (mathias.menninghaus@uos.de)
+ * 
+ */
+public class Fraction {
+
+   /**
+    * The regular expression that defines the String representation of a
+    * Fraction.
+    */
+   public static final String REGEX = "-?\\d+/\\d*[1-9]\\d*";
+
+   /**
+    * Creates greatest common divisor for a and b.
+    * 
+    * @param a
+    * @param b
+    * @return the greatest common divisor for a and b.
+    */
+   public static int gcd(int a, int b) {
+      return b == 0 ? a : gcd(b, a % b);
+
+   }
+
+   /**
+    * Parses a Fraction from a String by using REGEX. Throws RuntimeException if String s does
+    * not contain valid Fraction.
+    * 
+    * @param s
+    *           String to be parsed
+    * @return parsed String as Fraction
+    * @throws RuntimeException
+    *            if String s is not a valid Fraction
+    */
+   public static Fraction parseFraction(String s) {
+      if (!s.matches(REGEX)) {
+         throw new RuntimeException("Parsing error");
+      }
+      String[] splitted = s.split("/");
+      return new Fraction(Integer.parseInt(splitted[0]),
+            Integer.parseInt(splitted[1]));
+   }
+
+   private int numerator;
+
+   private int denominator;
+
+   /**
+    * Creates a Fraction object with numerator and denominator 1, cancels the
+    * fraction with creation.
+    * 
+    * @param numerator
+    */
+   public Fraction(int numerator) {
+      this(numerator, 1);
+   }
+
+   /**
+    * Creates a Fraction object with numerator and denominator, cancels the
+    * fraction by creation. Denominator == 0 is not allowed.
+    * 
+    * @param numerator
+    * @param denominator
+    */
+   public Fraction(int numerator, int denominator) {
+      if (denominator == 0) {
+         throw new RuntimeException("denominator == 0 is not possible");
+      }
+
+      /*
+       * creates greatest common divisior.
+       */
+      int gcd = Fraction.gcd(numerator, denominator);
+      /*
+       * check sign, make denominator positive
+       */
+      if (denominator / gcd < 0) {
+         gcd *= -1;
+      }
+
+      this.numerator = numerator / gcd;
+
+      this.denominator = denominator / gcd;
+   }
+
+   /**
+    * Adds addend to this Fraction and return the result as a new Fraction.
+    * 
+    * @param addend
+    *           Fraction to be added
+    * @return the sum as a new Fraction
+    */
+   public Fraction add(Fraction addend) {
+      return new Fraction(this.numerator * addend.denominator
+            + this.denominator * addend.numerator, this.denominator
+            * addend.denominator);
+   }
+
+   /**
+    * Divides this Fraction with another Fraction
+    * 
+    * @param another
+    *           Fraction to divide this Fraction by
+    * @return quotient as a new Fraction
+    */
+   public Fraction divide(Fraction another) {
+      return new Fraction(this.numerator * another.denominator,
+            this.denominator * another.numerator);
+   }
+
+   /**
+    * 
+    * @return denominator of this Fraction
+    */
+   public int getDenominator() {
+      return this.denominator;
+   }
+
+   /**
+    * 
+    * @return numerator of this Fraction
+    */
+   public int getNumerator() {
+      return this.numerator;
+   }
+
+   /**
+    * Multiplies this Fraction with another Fraction
+    * 
+    * @param another
+    *           Fraction to multiply this Fraction with
+    * @return product as a new Fraction
+    */
+   public Fraction multiply(Fraction another) {
+      return new Fraction(this.numerator * another.numerator, this.denominator
+            * another.denominator);
+   }
+
+   /**
+    * Multiplies this Fraction with n.
+    * 
+    * @param n
+    *           factor to multiply with
+    * @return product as a new Fraction
+    */
+   public Fraction multiply(int n) {
+      return new Fraction(this.numerator * n, this.denominator);
+   }
+
+   /**
+    * Multiplies this Fraction with all other Fraction instances given by
+    * factors
+    * 
+    * @param factors
+    *           Fraction instances to multiply this Fraction with
+    * @return product as a new Fraction
+    */
+   public Fraction multiply(Fraction... factors) {
+      Fraction result = this;
+      for (int i = 0; i < factors.length; i++) {
+         result = result.multiply(factors[i]);
+      }
+      return result;
+   }
+   
+   /**
+    * Subtracts subtrahend from this Fraction an returns the result as a new
+    * Fraction.
+    * 
+    * @param subtrahend
+    *           to be subtracted Fraction
+    * @return the difference as a new Fraction
+    */
+   public Fraction subtract(Fraction subtrahend) {
+      return new Fraction(this.numerator * subtrahend.denominator
+            - this.denominator * subtrahend.numerator, this.denominator
+            * subtrahend.denominator);
+   }
+
+
+   /**
+    * Returns a string representation of this Fraction such as
+    * numerator/denominator.
+    * 
+    * @return This Fraction as a String
+    */
+   public String toString() {
+      return numerator + "/" + denominator;
+   }
+}
