6a804f548b057a78ad2f0168119c536efbc6d34c
tuto193 <carlos.a.parra.f@gmail.com>
2018-05-13 15:53:03 +0200

    Most main things are all Javadoced so far

--- blatt_04/Aufgabe3/BluRay.java
+++ blatt_04/Aufgabe3/BluRay.java
@@ -1,22 +1,50 @@
 package Aufgabe3;
-
+/**
+ * A BluRay is defined by its title and its director
+ */
 public class BluRay extends LibraryItem {
+    /**
+     * The defining attributes of the BluRay
+     */
     private String title;
     private String director;
 
+    /**
+     * In the constructor one must state the title and the director of the BluRay
+     * is to be instantiated
+     * 
+     * @param title a String that represents the title of this BluRay
+     * @param director a String that represents the director of this BluRay
+     */
     public BluRay( String title, String director ) {
         this.title = title;
         this.director = director;
     }
 
+    /**
+     * Returns the title String of this BluRay
+     * 
+     * @return title of this BluRay
+     */
     public String getTitle() {
         return this.title;
     }
 
+    /**
+     * Returns the director String of this BluRay
+     * 
+     * @return director of this BluRay 
+     */
     public String getDirector() {
         return this.director;
     }
 
+    /**
+     * Returns a brief description String of this BluRay, based on its title
+     * and its director
+     * 
+     * @return a brief description of this BluRay
+     */
     public String getDescription() {
         return "The movie \"" + this.title + " \" was directed by: " + this.director + "\n My mom said it was O.K. .";
     }
--- blatt_04/Aufgabe3/Book.java
+++ blatt_04/Aufgabe3/Book.java
@@ -6,22 +6,46 @@ package Aufgabe3;
  * @author Carlos A. Parra F.
  */
 public class Book extends LibraryItem {
+    /**
+     * The defining attributes of a Book
+     */
     private String title;
     private String author;
 
+    /**
+     * The Constructor sets the title and author attributes of the book
+     * 
+     * @param title a String that defines the books title
+     * @param author a String that defines the books author
+     */
     public Book ( String title, String author ) {
         this.title = title;
         this.author = author;
     }
 
+    /**
+     * Returns the title String of this book
+     * 
+     * @return the title of this book
+     */
     public String getTile() {
         return this.title;
     }
 
+    /**
+     * Return the author String of this book
+     * 
+     * @return the author of this book
+     */
     public String getAuthor() {
         return this.author;
     }
 
+    /**
+     * Returns a brief description of the book, using its title and author
+     * 
+     * @return a brief description of the book
+     */
     public String getDescription() {
         return "The Book \"" + this.title + "\" was written by: " + this.author + ". \n Pretty good :D. ";
     }
