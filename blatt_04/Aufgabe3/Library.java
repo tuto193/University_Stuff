d1731065d663ae4aa2b46c038d0a88a1011c686d
tuto193 <carlos.a.parra.f@gmail.com>
2018-05-13 19:01:48 +0200

    The coding has been completed

--- blatt_04/Aufgabe3/Library.java
+++ blatt_04/Aufgabe3/Library.java
@@ -1,4 +1,7 @@
 package Aufgabe3;
+
+import javax.management.RuntimeErrorException;
+
 /**
  * A class representing a simple library
  * @author Carlos A. Parra F.
@@ -23,18 +26,38 @@ public class Library {
      * @param item a LibraryItem Object
      */
     public void addItem( LibraryItem item ) {
+        this.inventory.append(item);
     }
 
     /**
-     * Deletes the given item from the myItems List of this
-     * library
+     * Deletes the given item from the Library's inventory
      */
     public void deleteItem( LibraryItem item ) {
+        // if the Library's inventory is empty, you might as well not check it
+        if( !inventory.isEmpty() ){
+            // a temporary list and a list of items that match the description given
+            List tmpList = new List(null);
 
+            // We want our loop limit to be a static number that won't be accidentally changed in the loop
+            int loopLimit = inventory.getLength();
+
+            // Check all the items inside the inventory, matching items will be terminated
+            // matchingItems
+            for( int i = 0; i < loopLimit; i++ ) {
+                if( inventory.getHead().equals(item) ) {
+                    inventory.pop();
+                } else {
+                    // We need to empty our inventory into the temporaryList in order to advance
+                    tmpList.prepend( inventory.pop() );
+                }
+            }        
+            // Make sure that our inventory is just as it was before
+            this.inventory = tmpList;
+        }
     }
 
     /**
-     * Returns a List of items, whose title
+     * Returns a List of items, whose description
      * matches the given text. (IT DOES NOT CHECK WHETHER THE ITEM IS AVAILABLE)
      * 
      * @param text a String against which the List of items
@@ -43,14 +66,19 @@ public class Library {
      *          the description given on text.
      */
     public List search( String text ) {
+        // if the Library's inventory is empty, you might as well not check it
         if( inventory.isEmpty() ) {
             return null;
         }
+        // a temporary list and a list of items that match the description given
         List tmpList = new List(null);
         List matchingItems = new List( null);
 
+        // We want our loop limit to be a static number that won't be accidentally changed in the loop
         int loopLimit = inventory.getLength();
 
+        // Check all the items inside the inventory, matching items will be copied over to the
+        // matchingItems
         for( int i = 0; i < loopLimit; i++ ) {
             // I could change it to look better directly from the abstract
             // class itself, and check for directors and all that...
@@ -60,8 +88,10 @@ public class Library {
             } else if( ( (BluRay) inventory.getHead() ).getDescription().toLowerCase().equals( text.toLowerCase() ) ) {
                 matchingItems.prepend( inventory.getHead() );
             }
+            // We need to empty our inventory into the temporaryList in order to advance
             tmpList.prepend( inventory.pop() );
         }        
+        // Make sure that our inventory is just as it was before
         this.inventory = tmpList;
 
         return matchingItems;
