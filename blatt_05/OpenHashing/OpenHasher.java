e1aa418bbe1dc21973764747e8d8bec4068c24eb
tuto193 <carlos.a.parra.f@gmail.com>
2018-05-27 13:24:49 +0200

    We can start to implement the whole thing now

--- blatt_05/OpenHashing/OpenHasher.java
+++ blatt_05/OpenHashing/OpenHasher.java
@@ -1,5 +1,35 @@
 package OpenHashing;
-
+import util.*; // Make sure that we are able to use the list that we created, while
+                // still maintaining order
+/**
+ * An OpenHasher contains an array of Lists, in which Objects are sorted
+ * according to their Hash Codes.
+ */
 public class OpenHasher<T> implements HashFunction, HashSet {
 
+    private GenericList<T>[] hashList;
+	@Override
+	public boolean contains(Object o) {
+		return false;
+	}
+
+	@Override
+	public boolean insert(Object o) {
+		return false;
+	}
+
+	@Override
+	public boolean delete(Object o) {
+		return false;
+	}
+
+	@Override
+	public boolean equals(Object o1, Object o2) {
+		return false;
+	}
+
+	@Override
+	public int hashCode(Object o) {
+		return 0;
+	}
 }
