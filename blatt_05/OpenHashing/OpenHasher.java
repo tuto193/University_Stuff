eeeb525ee225ea39364359d260705cf99b946902
tuto193 <carlos.a.parra.f@gmail.com>
2018-05-27 14:46:19 +0200

    Fast fertig

--- blatt_05/OpenHashing/OpenHasher.java
+++ blatt_05/OpenHashing/OpenHasher.java
@@ -5,31 +5,71 @@ import util.*; // Make sure that we are able to use the list that we created, wh
  * An OpenHasher contains an array of Lists, in which Objects are sorted
  * according to their Hash Codes.
  */
-public class OpenHasher<T> implements HashFunction, HashSet {
+public class OpenHasher implements HashSet {
+    /**
+     * The Array of lists, where our Lists with the HashCodes will be saved
+     */
+    private GenericList[] hashList;
+    private int magicNumber;
 
-    private GenericList<T>[] hashList;
+    /**
+     * The Default constructor creates an array of length 10
+     */
+    public OpenHasher() {
+        this( 10 );
+    }
+
+    /**
+     * This picky constructor allows for the creation of an array of a preffered length.
+     * The Lists themselves will be initialised here as well
+     */
+    public OpenHasher( int sayTheWord ) {
+        this.magicNumber = sayTheWord;
+        this.hashList = new GenericList[ magicNumber ];
+        for( int i = 0; i < hashList.length; i++ ) {
+            hashList[i] = new GenericList<Object>();
+        }
+    }
+
 	@Override
 	public boolean contains(Object o) {
+        int index = o.hashCode() % magicNumber;
+        while( !hashList[index].endpos() ) {
+            if( hashList[index].elem().equals(o) ) {
+                hashList[index].reset();
+                return true;
+            }
+            hashList[index].advance();
+        }
+        hashList[index].reset();
 		return false;
 	}
 
 	@Override
 	public boolean insert(Object o) {
+        int index = o.hashCode() % magicNumber;
+        if( !this.contains(o) ) {
+            hashList[index].add( o );
+            return true;
+        }
 		return false;
 	}
 
 	@Override
 	public boolean delete(Object o) {
+        int index = o.hashCode() % magicNumber;
+        if( this.contains( o ) ) {
+            while( !hashList[index].endpos() ) {
+                if( hashList[index].elem().equals( 0 ) ) {
+                    hashList[index].delete();
+                    hashList[index].reset();
+                    return true;
+                }
+                hashList[index].advance();
+            }
+        }
+        hashList[index].reset();
 		return false;
 	}
-
-	@Override
-	public boolean equals(Object o1, Object o2) {
-		return false;
-	}
-
-	@Override
-	public int hashCode(Object o) {
-		return 0;
-	}
+	
 }
