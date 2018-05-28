794d41f1c6ef4b4b7df5abf9f10eaef9531157b4
tuto193 <carlos.a.parra.f@gmail.com>
2018-05-28 08:12:37 +0200

    The testing and the docing should all be done by now

--- blatt_05/OpenHashing/HashingTest.java
+++ blatt_05/OpenHashing/HashingTest.java
@@ -1,9 +1,22 @@
 package OpenHashing;
 
 import OpenHashing.OpenHasher;
-
+/**
+ * A simple set of tests for the OpenHasher class.
+ * 
+ * @author Carlos A. Parra F.
+ */
 public class HashingTest {
 
+    /**
+     * Checks a test's results against its expected outcome., and provides an output
+     * accordingly. Throws a RuntimeException should the tests fail.
+     * 
+     * @param testName a String describing the name of the test, for easy tracking
+     * @param expectedOutcome the expected outcome of the test
+     * @param actualOutcome the actual outcome of the test.
+     * @throws RuntimeException if the outcomes don't match
+     */
     public static void testMethod( String testName, boolean expectedOutcome, boolean actualOutcome ) {
         if( expectedOutcome != actualOutcome ) {
             throw new RuntimeException("The test " +
@@ -26,8 +39,7 @@ public class HashingTest {
         // HASH CODE... therefore they should be stored in the same index/list inside the array.
         boolean TEST1 = testSubject.insert(testObjects);    // true because first
         boolean TEST2 = testSubject.insert(test1);          // true...
-        boolean TEST3 = testSubject.insert(test2);          // I think false
-        boolean TEST4 = testSubject.insert(test3);          // true, because new string
+        boolean TEST3 = testSubject.insert(test2);          // false
         boolean TEST5 = testSubject.insert(test4);          // false
         boolean TEST6 = testSubject.insert(test5);          // true
 
@@ -49,7 +61,7 @@ public class HashingTest {
         testMethod("TEST1", true, TEST1);
         testMethod("TEST2", true, TEST2);
         testMethod("TEST3", false, TEST3);
-        testMethod("TEST4", true, TEST4);
+        testMethod("TEST4", false, TEST4);
         testMethod("TEST5", true, TEST5);
         testMethod("TEST6", true, TEST6);
 
