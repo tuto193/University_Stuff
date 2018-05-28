785e59f34d59ce2fee29e8cfaa9bf1f26a3963fb
tuto193 <carlos.a.parra.f@gmail.com>
2018-05-28 07:15:04 +0200

    More tests.. wohoo

--- blatt_05/OpenHashing/HashingTest.java
+++ blatt_05/OpenHashing/HashingTest.java
@@ -4,10 +4,10 @@ import OpenHashing.OpenHasher;
 
 public class HashingTest {
 
-    public static void testMethod( String methodName, boolean expectedOutcome, boolean actualOutcome ) {
+    public static void testMethod( String testName, boolean expectedOutcome, boolean actualOutcome ) {
         if( expectedOutcome != actualOutcome ) {
-            throw new RuntimeException(" The method " +
-            methodName + " failed.")
+            throw new RuntimeException("The test " +
+            testName + " failed.");
         }
     }
 
@@ -31,6 +31,40 @@ public class HashingTest {
         boolean TEST5 = testSubject.insert(test4);          // false
         boolean TEST6 = testSubject.insert(test5);          // true
 
+        // Based on the results of the first tests, then all following tests should have the exact same results (same boolean values)
+        boolean containTest1 = testSubject.contains(testObjects);
+        boolean containTest2 = testSubject.contains(test1);
+        boolean containTest3 = testSubject.contains(test2);
+        boolean containTest4 = testSubject.contains(test3);
+        boolean containTest5 = testSubject.contains(test4);
+        boolean containTest6 = testSubject.contains(test5);
 
+        boolean deleteTest1 = testSubject.delete(testObjects);
+        boolean deleteTest2 = testSubject.delete(test1);
+        boolean deleteTest3 = testSubject.delete(test2);
+        boolean deleteTest4 = testSubject.delete(test3);
+        boolean deleteTest5 = testSubject.delete(test4);
+        boolean deleteTest6 = testSubject.delete(test5);
+
+        testMethod("TEST1", true, TEST1);
+        testMethod("TEST2", true, TEST2);
+        testMethod("TEST3", false, TEST3);
+        testMethod("TEST4", true, TEST4);
+        testMethod("TEST5", true, TEST5);
+        testMethod("TEST6", true, TEST6);
+
+        testMethod("containTest1", TEST1, containTest1);
+        testMethod("containTest2", TEST2, containTest2);
+        testMethod("containTest3", TEST3, containTest3);
+        testMethod("containTest4", TEST4, containTest4);
+        testMethod("containTest5", TEST5, containTest5);
+        testMethod("containTest6", TEST6, containTest6);
+
+        testMethod("deleteTest1", TEST1, deleteTest1);
+        testMethod("deleteTest2", TEST2, deleteTest2);
+        testMethod("deleteTest3", TEST3, deleteTest3);
+        testMethod("deleteTest4", TEST4, deleteTest4);
+        testMethod("deleteTest5", TEST5, deleteTest5);
+        testMethod("deleteTest6", TEST6, deleteTest6);
     }
 }
