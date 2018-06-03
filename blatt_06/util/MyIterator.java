62217ce5cf58507a4a4342bbe241e4e091677c16
tuto193 <carlos.a.parra.f@gmail.com>
2018-06-03 18:35:17 +0200

    I think it should work

--- blatt_06/util/MyIterator.java
+++ blatt_06/util/MyIterator.java
@@ -1,17 +1,54 @@
 package util;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class MyIterator implements Iterator {
     private MyEntry<E> prev;
     private MyEntry<E> curr;
-    private MyEntry<E> next;
     private MyList<E> list;
 
     public MyIterator( MyEntry<E> first, MyEntry<E> second, MyList<E> list ) {
         this.list = list;
         this.prev = null;
         this.curr = first;
-        this.next = second;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return this.curr != null;
+    }
+
+    @Override
+    public E next() {
+        // Show error if the List had changed
+        // And another error if there is no such next element
+        if( !hasNext() ) {
+            throw new NoSuchElementException();
+        }
+        E o = this.curr.o; // the next element
+        // Move our triad further
+        this.prev = this.curr;
+        this.curr = this.curr.next;
+
+        return o;
+    }
+
+    /**
+     * Returns the element that is deleted from the list
+     * (if any)
+     * 
+     * @return o Object pointed at by the Entry withing the list
+     * @throws NoSuchElementException if there is no element to be deleted
+     */
+    public E delete() {
+        if( !hasNext() ) {
+            throw new NoSuchElementException();
+        }
+        // the object to the taken out
+        E o = this.curr.o;
+        this.prev.next = this.curr.next;
+        this.curr = this.curr.next;
+        return o;
     }
 }
