4bdc7105a2b2454959033cb16f499f5fdbc4c797
tuto193 <carlos.a.parra.f@gmail.com>
2018-06-03 22:47:03 +0200

    Testklasse erweitern

--- blatt_06/util/MyListTest.java
+++ blatt_06/util/MyListTest.java
@@ -7,13 +7,26 @@ public class MyListTest {
 
    public static void main(String[] args) {
 
-      /*
-       * not absolute requirements
-       */
-      MyList<String> mine = new MyList<String>();
-      for( Iterator<String> it = mine.iterator(); it.hasNext(); ) {
-            it.remove();
-      }
+        /*
+        * not absolute requirements
+        */
+        MyList<String> mine = new MyList<String>();
+        mine.add( "a" );
+        mine.add( "b" );
+        mine.add( "c" );
+        String brokenUp = "This shit is long enough to make a list I hope...";
+        mine.add( "Potatoe" );
+
+        // Just add a bunch more Strings to make sure that our list has a nice ammount of stuff
+        for( int i = 0; i < brokenUp.length(); i++ ) {
+            String addMe = "" + brokenUp.charAt(i);
+            mine.add( addMe );
+        }
+        for( Iterator<String> it = mine.iterator(); it.hasNext(); ) {
+            if( it.next().equals("Potatoe") ) {
+                it.remove();
+            }
+        }
       
    }
 }
