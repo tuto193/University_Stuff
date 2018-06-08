770a73143d2c36eef55127d9d00d29e3f411cfb9
tuto193 <carlos.a.parra.f@gmail.com>
2018-06-08 18:25:19 +0200

    I think the whole thing is pretty much done, now we just need the writting part

--- blatt_07/src/Aufgabe1/CollectionTest.java
+++ blatt_07/src/Aufgabe1/CollectionTest.java
@@ -2,6 +2,8 @@ package Aufgabe1;
 
 import java.util.LinkedList;
 import java.util.Random;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 public class CollectionTest {
     // With how many objects do we want to test
@@ -33,7 +35,7 @@ public class CollectionTest {
     }
 
     public static void nanoTestList( boolean seeValues ) {
-        printTest( "List" );
+        printTest( "Tests for LinkedList" );
         LinkedList<String> testList = new LinkedList<String>();
         long[] times = new long[objectNumber];
         
@@ -97,9 +99,200 @@ public class CollectionTest {
 
         ///// REMOVE Testing starts HERE /////////////////////
 
+        long[] removeTests = new long[objectNumber];
+
+        for( int i = 0; i < removeTests.length; i++ ) {
+            long start = System.nanoTime();
+            testList.remove("" + i );
+            long result = System.nanoTime() - start;
+            removeTests[i] = result;
+        }
+
+        System.out.println("We test now the remove() method ");
+        testAverages(removeTests, removeTests.length);
+
+        if( seeValues ) {
+            System.out.println("These are all the remove() times: ");
+            for( int i = 0; i < removeTests.length; i++ ) {
+                System.out.println( removeTests[i] );
+            }
+        }
+    }
+
+    public static void nanoTestArray( boolean seeValues ) {
+        printTest( "Tests for ArrayList" );
+        ArrayList<String> testList = new ArrayList<String>();
+        long[] times = new long[objectNumber];
+        
+        //// ADD Testing starts HERE /////////////////////////
+        for( int i = 0; i < times.length; i++ ) {
+            String pushIn = "" + i;
+            long start = System.nanoTime();
+            testList.add(pushIn);
+            long result = System.nanoTime() - start;
+            times[i] = result; 
+        }
+
+        // We willl check the input times of the whole
+        System.out.println("These is the add() method"); 
+        testAverages( times, times.length );
+
+        if( seeValues ) {
+            System.out.println("This are all of the add() Times");
+            for( int i = 0; i < times.length; i++ ) {
+                System.out.println( times[i] );
+            }
+        }
+
+        ///// ADD Testing finishes HERE ///////////////
+
+        ///// CONTAINS Testing starts HERE ///////////
+        Random rand = new Random();
+        // The last 10 Tests, will check for elements that don't exist
+        // within the collection ( can be changed by modifying variable emptyTests)
+        long[] containTests = new long[ objectNumber / 20 ];
+
+        for( int i = 0; i < containTests.length; i++ ) {
+            int randomCheck;
+            if( i < containTests.length - emptyTests ) {
+                randomCheck = rand.nextInt(objectNumber);
+            } else {
+                randomCheck = rand.nextInt(objectNumber) + objectNumber;
+            }
+            long start = System.nanoTime();
+            testList.contains("" + randomCheck );
+            long result = System.nanoTime() - start;
+            containTests[i] = result;
+        }
+
+        // we will print the tests here
+        System.out.println("We test now the contains() method ");
+        testAverages( containTests, containTests.length - emptyTests );
+
+        if( seeValues ) {
+            System.out.println("These are all the contains() times: ");
+            for( int i = 0; i < containTests.length; i++ ) {
+                if( i < containTests.length - emptyTests ) {
+                    System.out.println("Actual test: " + containTests[i] );
+                } else {
+                    System.out.println("Empty test: " + containTests[i] );
+                }
+            }
+        }
+
+        ///// CONTAINS Testing finishes HERE /////////////////
+
+        ///// REMOVE Testing starts HERE /////////////////////
+
+        long[] removeTests = new long[objectNumber];
+
+        for( int i = 0; i < removeTests.length; i++ ) {
+            long start = System.nanoTime();
+            testList.remove("" + i );
+            long result = System.nanoTime() - start;
+            removeTests[i] = result;
+        }
+
+        System.out.println("We test now the remove() method ");
+        testAverages(removeTests, removeTests.length);
+
+        if( seeValues ) {
+            System.out.println("These are all the remove() times: ");
+            for( int i = 0; i < removeTests.length; i++ ) {
+                System.out.println( removeTests[i] );
+            }
+        }
+    }
+    
+    public static void nanoTestHashSet( boolean seeValues ) {
+        printTest( "Tests for HashSet" );
+        HashSet<String> testList = new HashSet<String>();
+        long[] times = new long[objectNumber];
+        
+        //// ADD Testing starts HERE /////////////////////////
+        for( int i = 0; i < times.length; i++ ) {
+            String pushIn = "" + i;
+            long start = System.nanoTime();
+            testList.add(pushIn);
+            long result = System.nanoTime() - start;
+            times[i] = result; 
+        }
+
+        // We willl check the input times of the whole
+        System.out.println("These is the add() method"); 
+        testAverages( times, times.length );
+
+        if( seeValues ) {
+            System.out.println("This are all of the add() Times");
+            for( int i = 0; i < times.length; i++ ) {
+                System.out.println( times[i] );
+            }
+        }
+
+        ///// ADD Testing finishes HERE ///////////////
+
+        ///// CONTAINS Testing starts HERE ///////////
+        Random rand = new Random();
+        // The last 10 Tests, will check for elements that don't exist
+        // within the collection ( can be changed by modifying variable emptyTests)
+        long[] containTests = new long[ objectNumber / 20 ];
+
+        for( int i = 0; i < containTests.length; i++ ) {
+            int randomCheck;
+            if( i < containTests.length - emptyTests ) {
+                randomCheck = rand.nextInt(objectNumber);
+            } else {
+                randomCheck = rand.nextInt(objectNumber) + objectNumber;
+            }
+            long start = System.nanoTime();
+            testList.contains("" + randomCheck );
+            long result = System.nanoTime() - start;
+            containTests[i] = result;
+        }
+
+        // we will print the tests here
+        System.out.println("We test now the contains() method ");
+        testAverages( containTests, containTests.length - emptyTests );
+
+        if( seeValues ) {
+            System.out.println("These are all the contains() times: ");
+            for( int i = 0; i < containTests.length; i++ ) {
+                if( i < containTests.length - emptyTests ) {
+                    System.out.println("Actual test: " + containTests[i] );
+                } else {
+                    System.out.println("Empty test: " + containTests[i] );
+                }
+            }
+        }
+
+        ///// CONTAINS Testing finishes HERE /////////////////
+
+        ///// REMOVE Testing starts HERE /////////////////////
+
+        long[] removeTests = new long[objectNumber];
+
+        for( int i = 0; i < removeTests.length; i++ ) {
+            long start = System.nanoTime();
+            testList.remove("" + i );
+            long result = System.nanoTime() - start;
+            removeTests[i] = result;
+        }
+
+        System.out.println("We test now the remove() method ");
+        testAverages(removeTests, removeTests.length);
+
+        if( seeValues ) {
+            System.out.println("These are all the remove() times: ");
+            for( int i = 0; i < removeTests.length; i++ ) {
+                System.out.println( removeTests[i] );
+            }
+        }
     }
 
     public static void main( String[] args ) {
-
+        boolean wantToSee = true;
+        nanoTestArray( wantToSee );
+        nanoTestList( wantToSee );
+        nanoTestHashSet( wantToSee );
     }
 }
