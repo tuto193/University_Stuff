b7a665eecd31969b2c2d75072ff2088fb44f0e1d
tuto193 <carlos.a.parra.f@gmail.com>
2018-06-10 12:32:32 +0200

    Just save it for the moment

--- blatt_07/src/Aufgabe2/MyVisitor.java
+++ blatt_07/src/Aufgabe2/MyVisitor.java
@@ -1,11 +1,27 @@
 package Aufgabe2;
 
-import util.*;
+import util.Visitor;
 
 public class MyVisitor<E> implements Visitor<E> {
+
+    /* public final static int SKIP = 1;
+    public final static int ONWARD = 0;
+    public final static int STOP = -1; */
+
+    public enum Step {
+        ONWARD, SKIP, STOP
+    }
+
+    Step toTake;
+
     @Override
     public boolean visit( E o ) {
-        System.out.println( o.toString() );
+        System.out.println( "Visited " + o.toString() );
         return true;
+    }
+
+    public int visit( E o ) {
+        System.out.println( "Visited " + o.toString() );
+        return Step;
     }
 }
